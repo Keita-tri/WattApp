f46ccd414ea4dd383fdb048737c29bd2d93c64dc
中川恵太 <nakagawakeita@ktan.local>
2022-01-28 21:25:08 +0900

    新しいクラスを参照して追加

--- hr.js
+++ hr.js
@@ -1,37 +1,20 @@
-const btn = document.getElementById('bt')
+import HeartRateSensor from "./heartRateSensor"
 
-const connect = async (props) => {
-    const device = await navigator.bluetooth.requestDevice({
-        filters:[{ services: ['heart_rate']}],
-        // filters:[{ services: ['cycling_power']}]
-    })
-    
-    const server = await device.gatt.connect()  // connection情報をgattServerに対して変更
-    const service = await server.getPrimaryService('heart_rate') //プライマリサービス情報を取得（uuid,name,など)(デバイス情報のサービスではない）
-    const char = await service.getCharacteristics('heart_rate_measurement')//心拍計機能のさらに心拍計測characteristicへアクセス)
-    
-    char.oncharacteristicvaluechanged = props.onChange
-    char.startNotifications()
-    console.log(device.name)
-    return char
-}
+device1 = new HeartRateSensor
 
-let hrData = new Array(200).fill(10)
+const btn = document.getElementById('bt1')
+const hrvalue = document.getElementById('hrvalue')
 
-const pp = 
-console.clear()
-btn.addEventListener('click', {
-    
-})
+const btn2 = document.getElementById('bt2')
+const hrvalue2 = document.getElementById('hrvalue2')
 
-function printHeartRate(event) {
-    const heartRate = event.target.value.getInt8(1)
-    const prev = hrData[hrData.length - 1]
-    hrData[hrData.length] = heartRate
-    hrData = hrData.slice(-200)
-    // let arrow = ''
-    // if (heartRate !== prev) arrow = heartRate > prev ? '⬆' : '⬇'
 
-    hr = document.getElementById('hrvalue')
-    hr.innerHTML = 'heartRate'
-}
+btn.addEventListener('click', function() {
+    hrvalue1.textContent = 'Breathe...';
+    heartRates = [];
+    heartRateSensor.connect()
+    .then(() => heartRateSensor.startNotificationsHeartRateMeasurement().then(handleHeartRateMeasurement))
+    .catch(error => {
+      statusText.textContent = error;
+    });
+  });
--- test.js
+++ test.js
@@ -1,27 +1,5 @@
-const device1 = await navigator.bluetooth.requestDevice({
-    filters:[{ services: ['heart_rate']}],
-    // filters:[{ services: ['cycling_power']}]
-})
+import HeartRateSensor from "./heartRateSensor"
 
-const server = await device1.gatt.connect()  // connection情報をgattServerに対して変更
-const service = await server.getPrimaryService('heart_rate') //プライマリサービス情報を取得（uuid,name,など)(デバイス情報のサービスではない）
+device1 = new HeartRateSensor
 
-const char = await service.getCharacteristics('heart_rate_measurement')//心拍計機能のさらに心拍計測characteristicへアクセス)
-
-console.log(server)
-console.log(service)
-console.log(char)
-
-
-
-function printHeartRate(event) {
-const heartRate = event.target.value.getInt8(1)
-const prev = hrData[hrData.length - 1]
-hrData[hrData.length] = heartRate
-hrData = hrData.slice(-200)
-let arrow = ''
-if (heartRate !== prev) arrow = heartRate > prev ? '⬆' : '⬇'
-console.clear()
-console.graph(hrData)
-console.log(`%c\n💚 ${heartRate} ${arrow}`, 'font-size: 24px;', '\n\n(To disconnect, refresh or close tab)\n\n')
-}
+console.log(device1)
